7551488edc09aed41190135c6066152beb1aa5a4
Christine Altmann <christine.altmann@nivohub.com>
2019-11-17 13:21:16 +0000

    step 7 complete

--- server.js
+++ server.js
@@ -1,81 +1,10 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring');
 
+const handler = require('./src/handler.js');
 
-// var message = 'I am so happy to be part of the Node Girls workshop!';
-const handler = (request, response) => {
-    const endpoint = request.url;
-    console.log(endpoint);
+const server = http.createServer(handler);
 
-    const method = request.method;
-    console.log(method);
-
-    if (endpoint === "/") {
-        response.writeHead(200, {"Content-Type": "text/html"});
-
-        fs.readFile(__dirname + '/public/index.html', function (error, file) {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        });
-    } else if (endpoint === "/node") {
-        response.writeHead(200, {"Content-Type": "text/html"})
-        response.write("wow node time");
-        response.end();
-    } else if (endpoint === "/girls") {
-        response.writeHead(200, {"Content-Type": "text/html"})
-        response.write("node girls just wanna have fun");
-        response.end();
-    } else if (endpoint === '/create-post') {
-
-        message = "";
-
-        request.on("data", function(data) {
-            message += data;
-        });
-
-        request.on("end", function () {
-            response.writeHead(302, {"Location": "/"});
-            message = querystring.parse(message);
-            console.log(message.blogpost);
-            response.end();
-        });
-
-    } else {
-        const extension = endpoint.split('.')[1];
-        const extensionType = {
-            html: 'text/html',
-            css: 'text/css',
-            js: 'application/javascript',
-            ico: 'image/x-icon',
-            jpg: "image/jpeg",
-        };
-        const filePath = path.join(__dirname + '/public/' + endpoint);
-        fs.readFile(filePath, (error, file) => {
-                if (error) {
-                    console.log(error);
-                    response.writeHead(404, {'Content-Type': 'text/html'});
-                    response.end('<h1>404 file not found</h1>');
-                } else {
-                    response.writeHead(200, {'Content-Type': extensionType[extension]});
-                    response.end(file);
-                }
-            }
-        );
-}
-// else {
-//         response.writeHead(404, { 'Content-Type': 'text/html' });
-//         response.end('<h1>404 file not found</h1>');
-//      }
-}
-
-var server = http.createServer(handler);
-
-server.listen(3000, function () {
+server.listen(3000, () => {
 
     console.log("Server is listening on port 3000. Ready to accept requests!");
 });
