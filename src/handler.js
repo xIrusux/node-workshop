d38c8521769371d6fbe11b1d17d843d24f81feda
Christine Altmann <christine.altmann@nivohub.com>
2019-11-17 14:04:45 +0000

    step 10 complete

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs');
 var querystring = require('querystring');
 
@@ -11,31 +10,72 @@ function handler (request, response) {
         var pathToIndex = __dirname + '/../public/index.html';
 
         fs.readFile(pathToIndex, function (error, file) {
+
+            if (error) {
+                throw error;
+            }
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(file);
             response.end();
         });
-    } else if (request.method === 'POST') {
+    } else if (endpoint === '/add-post') {
 
         var data = "";
 
         request.on("data", function(chunk) {
+
             data += chunk;
         });
 
         request.on("end", function () {
-            response.writeHead(302, {"Location": "/"});
-            var convertedData = querystring.parse(data);
-            response.end();
+
+            var newPost = querystring.parse(data);
+
+            fs.readFile(__dirname + "/posts.json", "utf8", function (error, fileData) {
+
+                if (error) {
+                    console.log(error);
+                }
+                var blogposts = JSON.parse(fileData);
+                var currentTime = Date.now();
+
+                blogposts[currentTime] = newPost.blogpost;
+
+                fs.writeFile(__dirname + '/posts.json', JSON.stringify(blogposts, null, 4), function (error) {
+
+                    if (error) {
+                        console.log(error);
+                    }
+                    response.writeHead(302, {"Location" : "/"});
+                    response.end();
+                });
+            });
         });
 
+    } else if (endpoint === '/get-posts'){
+
+        var pathToJSON = __dirname + '/posts.json';
+
+        fs.readFile(pathToJSON, function (error, blogposts) {
+
+            if (error) {
+                throw error;
+            }
+            response.writeHead(200, {"Content-Type": "text/json"});
+            response.write(blogposts);
+            response.end();
+        });
     } else {
+
         var pathToFile = __dirname + '/../public' + endpoint;
         var fileExtensionArray = endpoint.split('.');
         var fileExtension = fileExtensionArray[1];
 
         fs.readFile(pathToFile, function (error, file) {
 
+            if (error) {
+                throw error;
+            }
             response.writeHead(200, { "Content-Type": "text/" + fileExtension });
             response.write(file);
             response.end();
